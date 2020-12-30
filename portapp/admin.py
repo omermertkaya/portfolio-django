8f13c970b93cbe9431ecf20dab1316e6cf91b651
omermertkaya <l1421012018@stud.sdu.edu.tr>
2020-12-30 23:41:40 +0300

    mert

--- portapp/admin.py
+++ portapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import PostHome2,PostHome3,PostHome4,Story_Bolumu, Work_Bolumu
 
 # Register your models here.
+admin.site.register(PostHome2)
+admin.site.register(PostHome3)
+admin.site.register(PostHome4)
+admin.site.register(Story_Bolumu)
+admin.site.register(Work_Bolumu)
